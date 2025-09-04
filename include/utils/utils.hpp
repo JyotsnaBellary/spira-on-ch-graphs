f0bd0af9e9eafc4ae7b9e50d475b7e2ec4bc2c71
jyotsna <jyotsna.bellary@uni-konstanz.de>
2025-09-04 06:33:38 +0200

    format

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -3,25 +3,18 @@
 #include "core/types.hpp"
 #include <filesystem>
 
-class Utils {
-    public:
-        Utils() = default;
-        vector<NodeId> independent_nodes(const Graph& graph, NodePQ& pq,
-                                                   int delta,
-                                                   const vector<int>& score);
-        Dist sat_add(Dist a, Dist b);
-        static bool is_local_min(NodeId u, const Graph& g, const vector<int>& s);
+class Utils
+{
+public:
+    Utils() = default;
+    vector<NodeId> independent_nodes(const Graph &graph, NodePQ &pq,
+                                     int delta,
+                                     const vector<int> &score);
+    Dist sat_add(Dist a, Dist b);
+    static bool is_local_min(NodeId u, const Graph &g, const vector<int> &s);
 
-        filesystem::path run_kahip(const char* out_name = "level1");
-        int compute_active_median(const Graph& g,
-                          const vector<int>& score,
-                          vector<int>& scratch);
-
-
+    filesystem::path run_kahip(const char *out_name = "level1");
+    int compute_active_median(const Graph &g,
+                              const vector<int> &score,
+                              vector<int> &scratch);
 };
-
-
-// class GraphUtils {
-//     public:
-// };
-
